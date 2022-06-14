e1de910b3bd6cc1d2012cf18fd6cbac97099a7f4
Nados12 <74115346+Nados12@users.noreply.github.com>
2022-06-14 12:09:57 +0300

    firebase-config.js

--- web/src/firebase-config.js
+++ web/src/firebase-config.js
@@ -1,20 +1,21 @@
-/**
- * To find your Firebase config object:
- * 
- * 1. Go to your [Project settings in the Firebase console](https://console.firebase.google.com/project/_/settings/general/)
- * 2. In the "Your apps" card, select the nickname of the app for which you need a config object.
- * 3. Select Config from the Firebase SDK snippet pane.
- * 4. Copy the config object snippet, then add it here.
- */
-const config = {
-  /* TODO: ADD YOUR FIREBASE CONFIGURATION OBJECT HERE */
-};
+// Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-export function getFirebaseConfig() {
-  if (!config || !config.apiKey) {
-    throw new Error('No Firebase configuration object provided.' + '\n' +
-    'Add your web app\'s configuration object to firebase-config.js');
-  } else {
-    return config;
-  }
-}
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyB5NVPF8vdAA49g3v6bvpyJWMaaKcHrdeU",
+    authDomain: "friendlychat-ed1f4.firebaseapp.com",
+    projectId: "friendlychat-ed1f4",
+    storageBucket: "friendlychat-ed1f4.appspot.com",
+    messagingSenderId: "864335557991",
+    appId: "1:864335557991:web:50f55519d6f3e9b2ea6bc2",
+    measurementId: "G-2BL94D4WYG"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
