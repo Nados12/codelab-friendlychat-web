5ade3bca7abaa2db3afa0d8b25b8e2885bd9e93f
Nados12 <74115346+Nados12@users.noreply.github.com>
2022-06-17 13:02:51 +0300

    Update firebase-config.js

--- web-start/src/firebase-config.js
+++ web-start/src/firebase-config.js
@@ -7,7 +7,14 @@
  * 4. Copy the config object snippet, then add it here.
  */
 const config = {
-  /* TODO: ADD YOUR FIREBASE CONFIGURATION OBJECT HERE */
+  apiKey: "API_KEY",
+  authDomain: "PROJECT_ID.firebaseapp.com",
+  databaseURL: "https://PROJECT_ID.firebaseio.com",
+  projectId: "PROJECT_ID",
+  storageBucket: "PROJECT_ID.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID",
+  measurementId: "G-MEASUREMENT_ID",
 };
 
 export function getFirebaseConfig() {
